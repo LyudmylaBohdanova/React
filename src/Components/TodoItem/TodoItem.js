338bee0edaf375deadc96ad87e940927665c09be
LyudmylaBogdanova <kroshkabubon@gmail.com>
2020-05-02 18:14:04 +0300

    change Checked

--- src/Components/TodoItem/TodoItem.js
+++ src/Components/TodoItem/TodoItem.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import './TodoItem.css';
 
-function TodoItem(props) {
-  return (
-    <div className="TodoItem">
-      <label class="container labelTode" for={props.info.id}>{props.info.text}
-        <input type="checkbox" id={props.info.id} checked={props.info.completed}></input>
-        <span class="checkmark"></span>
-      </label>
-    </div>
-    
-  );
-}
+ class TodoItem extends React.Component{
+   constructor(props){
+    super(props);
+    this.state = {
+      completed : props.info.completed,
+      id : props.info.id,
+      text : props.info.text,
+    };
+    this.handleChange = this.handleChange.bind(this);
+   }
+  handleChange(){
+    this.setState({
+      completed : !this.state.completed
+    });
+  };
+  render(){
+    return (
+      <div className="TodoItem">
+        <label class="container labelTode" for={this.state.id}>{this.state.text}
+          <input type="checkbox" id={this.state.id} checked={this.state.completed} onChange={() => this.handleChange()}></input>
+          <span class="checkmark"></span>
+        </label>
+      </div>
+    );
+  };
+ };
 
 export default TodoItem;
