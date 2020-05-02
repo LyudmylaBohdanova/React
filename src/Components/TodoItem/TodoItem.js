7b26145a2107ebf1810e14700d08c3a0435f97b6
LyudmylaBogdanova <kroshkabubon@gmail.com>
2020-05-02 16:47:01 +0300

    itemUpdate

--- src/Components/TodoItem/TodoItem.js
+++ src/Components/TodoItem/TodoItem.js
@@ -3,10 +3,6 @@ import './TodoItem.css';
 
 function TodoItem(props) {
   return (
-    // <div className="TodoItem">
-    //     <input className="inputTode" type="checkbox" id={props.info.id} checked={props.info.completed}></input>
-    //     <label className="labelTode" for={props.info.id}>{props.info.text}</label>
-    // </div>
     <div className="TodoItem">
       <label class="container labelTode" for={props.info.id}>{props.info.text}
         <input type="checkbox" id={props.info.id} checked={props.info.completed}></input>
